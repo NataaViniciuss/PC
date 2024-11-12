9c2aa3468336e9ea1cca3cef510e4fd461b70060
NataaViniciuss <natavinicius.nata@gmail.com>
2024-11-12 18:15:25 -0300

    novopopup

--- desktop/time.ts
+++ desktop/time.ts
@@ -21,3 +21,37 @@ function time() {
 }
 
 setInterval(time);
+
+function effectHover() {
+    const DivRestart = document.querySelector('.restart') as HTMLDivElement;
+    const DivSuspend = document.querySelector('.suspend') as HTMLDivElement;
+    const DivDesligar = document.querySelector('.desligar') as HTMLDivElement;
+    const iconRestart = document.getElementById('icon-restart') as HTMLImageElement;
+    const iconSuspend = document.getElementById('icon-suspend') as HTMLImageElement;
+    const iconDesligar = document.getElementById('icon-desligar') as HTMLImageElement;
+
+    DivRestart.addEventListener('mouseenter', () => {
+        iconRestart.src = 'img/icon/resestart.png';
+    });
+
+    DivRestart.addEventListener('mouseleave', () => {
+        iconRestart.src = 'img/icon/restart-icon.png';
+    });
+
+    DivSuspend.addEventListener('mouseenter', () => {
+        iconSuspend.src = 'img/icon/suspend.png';
+    });
+
+    DivSuspend.addEventListener('mouseleave', () => {
+        iconSuspend.src = 'img/icon/suspend-origin.png';
+    });
+
+    DivDesligar.addEventListener('mouseenter', () => {
+        iconDesligar.src = 'img/icon/desligar.png';
+    });
+
+    DivDesligar.addEventListener('mouseleave', () => {
+        iconDesligar.src = 'img/icon/powerof-origin.png';
+    });
+}
+effectHover();
